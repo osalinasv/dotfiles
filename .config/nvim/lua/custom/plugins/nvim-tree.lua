1e7ea4949e5854e19d62dbf71b4170d24275f78a
Omar Salinas <osalinasv@outlook.com>
2023-11-24 21:18:21 -0700

    add nvim-surround to nvim

--- .config/nvim/lua/custom/plugins/nvim-tree.lua
+++ .config/nvim/lua/custom/plugins/nvim-tree.lua
@@ -48,8 +48,7 @@ return {
 
     local keymap = vim.keymap
 
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file explorer" })
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
   end,
