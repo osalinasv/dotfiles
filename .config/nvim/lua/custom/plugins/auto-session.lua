bcc95aea98ac817cf9e1ff9ba4f47b26aaa03650
Omar Salinas <osalinasv@outlook.com>
2023-11-25 13:57:21 -0700

    improve various keymaps

--- .config/nvim/lua/custom/plugins/auto-session.lua
+++ .config/nvim/lua/custom/plugins/auto-session.lua
@@ -2,15 +2,22 @@ return {
   "rmagatti/auto-session",
   event = "VeryLazy",
   config = function()
+    local function change_nvim_tree_dir()
+      local api = require("nvim-tree.api")
+      api.tree.change_root(vim.fn.getcwd())
+      api.tree.open({ find_file = true })
+    end
+
     require("auto-session").setup({
       log_level = "error",
-      auto_restore_enabled = false,
-      auto_session_suppress_dirs = { "~/", "~/Code", "~/Downloads", "~/Documents", "~/Desktop" },
+      post_restore_cmds = { change_nvim_tree_dir },
+      pre_save_cmds = { "NvimTreeClose" },
+      post_save_cmds = { "NvimTreeOpen" },
     })
 
     local keymap = vim.keymap
 
     keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })
-    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" })
+    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for cwd" })
   end,
 }
--- .config/nvim/lua/custom/plugins/bufferline.lua
+++ .config/nvim/lua/custom/plugins/bufferline.lua
@@ -4,5 +4,13 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  opts = {},
+  config = function()
+    require("bufferline").setup()
+
+    local keymap = vim.keymap
+
+    keymap.set("n", "<Tab>", ":BufferLineCycleNext<cr>", { desc = "Go to next buffer" })
+    keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<cr>", { desc = "Go to previous buffer" })
+    keymap.set("n", "<leader>x", ":enew<bar>bd #<CR>", { desc = "Close current buffer" })
+  end,
 }
