4ffd63c4ecb561fbfa111b727d9fdd5734fbb621
Omar Salinas <osalinasv@outlook.com>
2023-12-22 10:39:48 -0700

    wrap ohmyposh in trycatch

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,11 @@
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/amro.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/half-life.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/json.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/spaceship.omp.json" | Invoke-Expression
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tokyonight_storm.omp.json" | Invoke-Expression
-Import-Module git-aliases -DisableNameChecking
-Import-Module DirColors
+[Console]::OutputEncoding = [Text.Encoding]::UTF8
+
+try {
+  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tokyonight_storm.omp.json" | Invoke-Expression
+
+  Import-Module git-aliases -DisableNameChecking
+  Import-Module DirColors
+} finally {}
 
 # Path Aliases
 $global:NVIM = "$HOME\AppData\Local\nvim"
