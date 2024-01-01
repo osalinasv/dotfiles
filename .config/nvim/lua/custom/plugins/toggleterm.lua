6ad77acee2a55303c0c7fa67fa03fe51a90cee7c
Omar Salinas <osalinasv@outlook.com>
2024-01-01 08:43:34 -0700

    add toggleterm to nvim

---
+++ .config/nvim/lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,20 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  lazy = true,
+  event = "VeryLazy",
+  config = function()
+    require("toggleterm").setup({
+      open_mapping = [[<c-t>]],
+      direction = "horizontal",
+      close_on_exit = true,
+    })
+
+    local keymap = vim.keymap
+    local opts = { buffer = 0 }
+
+    keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+    keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+    keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  end,
+}
