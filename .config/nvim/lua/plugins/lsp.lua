79650d0d821a35e54ac6a3ebaf1c9ccdfa4e070c
Omar Salinas <osalinasv@outlook.com>
2024-01-27 19:00:34 -0700

    remove csharp lsp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -12,24 +12,16 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      { "Decodetalkers/csharpls-extended-lsp.nvim", lazy = true },
-    },
     ---@class PluginLspOpts
-    opts = function(_, opts)
-      opts.servers = vim.tbl_deep_extend("force", opts.servers, {
+    opts = {
+      ---@type lspconfig.options
+      servers = {
         astro = {},
         cssls = {},
         html = {},
         tailwindcss = {},
         tsserver = {},
-        csharp_ls = {
-          handlers = {
-            ["textDocument/definition"] = require("csharpls_extended").handler,
-            ["textDocument/typeDefinition"] = require("csharpls_extended").handler,
-          },
-        },
-      })
-    end,
+      },
+    },
   },
 }
