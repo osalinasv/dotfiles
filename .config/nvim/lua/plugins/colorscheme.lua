942f3990d0990b2117455c77fed58ffcb09f877b
Omar Salinas <osalinasv@outlook.com>
2024-06-14 18:49:51 -0600

    remove catppucin and bring back todo-comments.nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,5 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      flavour = "mocha",
-    },
-  },
-  {
     "oxfist/night-owl.nvim",
     lazy = true,
     priority = 1000,
--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,10 +1,9 @@
 return {
+  { "catppuccin/nvim", name = "catppuccin", enabled = false },
   { "dstein64/vim-startuptime", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
   { "folke/noice.nvim", enabled = false },
-  { "folke/todo-comments.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
 }
