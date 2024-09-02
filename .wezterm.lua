b1659616a7dd2e354e7c9a10860564610ab401d3
Omar Salinas <osalinasv@outlook.com>
2024-09-01 21:56:23 -0600

    add macos specific wezterm configs

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,18 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 
+local is_windows = function()
+	return wezterm.target_triple:find("windows") ~= nil
+end
+
+local is_darwin = function()
+	return wezterm.target_triple:find("darwin") ~= nil
+end
+
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrains Mono")
-config.font_size = 12
+config.font_size = is_darwin() and 15 or 12
 
 config.color_scheme = "Tokyo Night"
 
@@ -14,7 +22,22 @@ config.hide_tab_bar_if_only_one_tab = false
 config.tab_and_split_indices_are_zero_based = true
 config.tab_bar_at_bottom = true
 
-config.default_prog = { "pwsh.exe", "-NoLogo" }
+if is_windows() then
+	config.default_prog = { "pwsh.exe", "-NoLogo" }
+end
+
+config.keys = {
+	{
+		key = "LeftArrow",
+		mods = "CMD",
+		action = wezterm.action({ SendString = "\x1bb" }),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD",
+		action = wezterm.action({ SendString = "\x1bf" }),
+	},
+}
 
 wezterm.on("gui-startup", function()
 	mux.spawn_window({ width = 120, height = 40 })
