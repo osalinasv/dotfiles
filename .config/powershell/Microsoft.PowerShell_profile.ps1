f329a168e066f0602253a113fd5c2fd57edb5b7c
Omar Salinas <osalinasv@outlook.com>
2024-11-24 12:18:08 -0700

    better powershell aliases

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,32 +1,33 @@
 [Console]::OutputEncoding = [Text.Encoding]::UTF8
 
+$Env:BAT_STYLE = "plain,numbers"
+#$Env:BAT_THEME = "tokyonight_night"
+
 Set-Variable MaximumHistoryCount 8192
 
-Set-Alias c cd
-Set-Alias l ls
-Set-Alias cl clear
+if (Get-Command eza -ErrorAction SilentlyContinue) {
+  Set-Alias ls eza
+}
+
+if (Get-Command bat -ErrorAction SilentlyContinue) {
+  Set-Alias cat bat
+}
 
 Set-Alias g git
 Set-Alias gg lazygit
 
-Set-Alias v nvim
 Set-Alias vim nvim
-
-$Env:BAT_STYLE = "plain,numbers"
-Set-Alias cat bat
 
 function ** {
   fzf --preview="bat --style=plain,numbers --color=always {}" --border=rounded $args
-}
-
-function la {
-  Get-ChildItem -Force $args
 }
 
 try {
   Import-Module git-aliases -DisableNameChecking
 
   Invoke-Expression (& { (zoxide init powershell | Out-String) })
+  Set-Alias cd z -Option AllScope
+
   Invoke-Expression (&starship init powershell)
 } finally {}
 
