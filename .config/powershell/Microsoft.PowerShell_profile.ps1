5c6c3ed1d38b96c41feb3e7ceaa6db9c3671ade8
Omar Salinas <osalinasv@outlook.com>
2025-02-15 19:30:58 -0700

    add git snippets to powershell

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -21,11 +21,121 @@ Set-Alias gg lazygit
 
 Set-Alias vim nvim
 
-function _run_fzf {
-  fzf --preview="bat --style=plain,numbers --color=always {}" --border=rounded $args
+if (Get-Command fzf -ErrorAction SilentlyContinue) {
+  function _run_fzf {
+    fzf --preview="bat --style=plain,numbers --color=always {}" --border=rounded $args
+  }
+
+  Set-Alias ** _run_fzf
 }
 
-Set-Alias ** _run_fzf
+function git-cleanup {
+  git clean -dfx .
+  git restore .
+}
+
+function git-tidy {
+  param (
+    [int]$commits = 1
+  )
+
+  git rebase -i HEAD~$commits
+}
+
+function git-log {
+  git log --graph --oneline --abbrev-commit $args
+}
+
+function git-commit {
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$message
+  )
+
+  git add .
+  git commit -m $message
+}
+
+function git-amend {
+  git add .
+  git commit --amend --no-edit
+}
+
+function git-stash {
+  param (
+    [Parameter(Mandatory=$false)]
+    [string]$message = "TEMP"
+  )
+
+  git stash push -u -m "$(Get-Date -Format "yyyy/MM/dd HH:mm"): $message"
+}
+
+function git-rebase {
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$branch
+  )
+
+  $currentBranch = git branch --show-current
+  $hasChanges = git status --porcelain
+
+  $stashed = $false
+  $canPop = $false
+
+  try {
+    if ($hasChanges) {
+      git-stash "Auto-stash before rebase of $currentBranch onto $branch"
+
+      if ($LASTEXITCODE -ne 0) {
+        throw "Failed to stash before rebase"
+      }
+
+      $stashed = $true
+      $canPop = $true
+    }
+
+    git fetch origin
+
+    if ($LASTEXITCODE -ne 0) {
+      throw "Failed to fetch branches"
+    }
+
+    git switch $branch
+
+    if ($LASTEXITCODE -ne 0) {
+      throw "Failed to switch to $branch"
+    }
+
+    $canPop = $false
+    git pull --ff-only
+
+    if ($LASTEXITCODE -ne 0) {
+      throw "Failed to pull $branch"
+    }
+
+    git switch $currentBranch
+
+    if ($LASTEXITCODE -ne 0) {
+      throw "Failed to switch back to $currentBranch"
+    }
+
+    $canPop = $true
+    git rebase $branch
+
+    if ($LASTEXITCODE -ne 0) {
+      $canPop = $false
+      throw "Failed to rebase $currentBranch onto $branch"
+    }
+  } catch {
+    Write-Host $_ -ForegroundColor DarkRed
+  } finally {
+    if ($stashed -and $canPop) {
+      git stash pop
+    } elseif ($stashed) {
+      Write-Host "Auto-stash remains pending due to error" -ForegroundColor DarkYellow
+    }
+  }
+}
 
 try {
   Import-Module git-aliases -DisableNameChecking
