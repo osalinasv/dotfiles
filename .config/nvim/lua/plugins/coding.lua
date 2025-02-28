601e5ecc089de40f77450ddb9efd64101ab39a42
Omar Salinas <osalinasv@outlook.com>
2025-02-28 12:36:12 -0700

    remove custom up-down keymaps from blink for now

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -6,8 +6,6 @@ return {
         preset = "super-tab",
         ["<C-k>"] = { "select_prev", "fallback" },
         ["<C-j>"] = { "select_next", "fallback" },
-        ["<C-u>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-d>"] = { "scroll_documentation_down", "fallback" },
         ["<C-q>"] = { "hide", "fallback" },
       },
     },
