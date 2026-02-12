a1456d706dc082456f474fa09cedacbb2844d019
Omar Salinas <osalinasv@outlook.com>
2026-02-11 19:43:45 -0700

    remove kanagawa theme from nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,25 +5,4 @@ return {
       style = "night",
     },
   },
-  {
-    "rebelot/kanagawa.nvim",
-    opts = {
-      theme = "wave",
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none",
-            },
-          },
-        },
-      },
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "kanagawa",
-    },
-  },
 }
