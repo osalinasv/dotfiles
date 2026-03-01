ca85b95bd98fbd9605ef143a6e91272005061362
Omar Salinas <osalinasv@outlook.com>
2026-02-28 21:13:09 -0700

    better base vim options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,40 +2,78 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
+-- Key mappings
+vim.g.mapleader = " " -- Set leader key to space
+vim.g.maplocalleader = " " -- Set local leader key (NEW)
 
+-- Basic settings
+vim.opt.number = true -- Line numbers
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.cursorline = true -- Highlight current line
+vim.opt.wrap = false -- Don't wrap lines
+vim.opt.scrolloff = 10 -- Keep 10 lines above/below cursor
+vim.opt.sidescrolloff = 8 -- Keep 8 columns left/right of cursor
+
+-- Indentation
+vim.opt.tabstop = 2 -- Tab width
+vim.opt.shiftwidth = 2 -- Indent width
+vim.opt.softtabstop = 2 -- Soft tab stop
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.smartindent = true -- Smart auto-indenting
+vim.opt.autoindent = true -- Copy indent from current line
+
+-- Search settings
+vim.opt.ignorecase = true -- Case insensitive search
+vim.opt.smartcase = true -- Case sensitive if uppercase in search
+vim.opt.hlsearch = false -- Don't highlight search results
+vim.opt.incsearch = true -- Show matches as you type
+
+-- Visual settings
+vim.opt.termguicolors = true -- Enable 24-bit colors
+vim.opt.signcolumn = "yes" -- Always show sign column
+vim.opt.colorcolumn = "80" -- Show column at 80 characters
+vim.opt.showmatch = true -- Highlight matching brackets
+vim.opt.matchtime = 2 -- How long to show matching bracket
+vim.opt.completeopt = "menuone,noinsert,noselect" -- Completion options
+vim.opt.conceallevel = 0 -- Don't hide markup
+vim.opt.concealcursor = "" -- Don't hide cursor line markup
+vim.opt.synmaxcol = 300 -- Syntax highlighting limit
+vim.opt.fillchars = { eob = " " } -- Hide ~ on empty lines
+vim.opt.listchars:append({
+  lead = "·",
+  trail = "·",
+  nbsp = "␣",
+  tab = "› ",
+})
+
+-- Behavior settings
+vim.opt.hidden = true -- Allow hidden buffers
+vim.opt.errorbells = false -- No error bells
+vim.opt.backspace = "indent,eol,start" -- Better backspace behavior
+vim.opt.autochdir = false -- Don't auto change directory
+vim.opt.iskeyword:append("-") -- Treat dash as part of word
+vim.opt.path:append("**") -- include subdirectories in search
+vim.opt.selection = "exclusive" -- Selection behavior
+vim.opt.mouse = "a" -- Enable mouse support
+vim.opt.clipboard:append("unnamedplus") -- Use system clipboard
+vim.opt.modifiable = true -- Allow buffer modifications
+vim.opt.encoding = "UTF-8" -- Set encoding
+
+-- Plugin settings
 vim.g.lazyvim_prettier_needs_config = false
-vim.g.mapleader = " "
 vim.g.snacks_animate = false
 
-vim.o.smartcase = true
-
-opt.breakindent = true
-opt.colorcolumn = "80"
-opt.conceallevel = 0
-opt.cursorline = true
-opt.expandtab = true
-opt.listchars:append({ lead = "·", trail = "·", nbsp = "␣", tab = "› " })
-opt.mouse = "a"
-opt.scrolloff = 8
-opt.sidescrolloff = 8
-opt.signcolumn = "yes"
-opt.smartindent = true
-opt.softtabstop = 2
-opt.tabstop = 2
-opt.wrap = false
-
 if vim.uv.os_uname().sysname:find("Windows") then
-  opt.shell = "pwsh"
-  opt.shellcmdflag = table.concat({
+  vim.opt.shell = "pwsh"
+  vim.opt.shellcmdflag = table.concat({
     "-NoLogo",
     "-NoProfile",
     "-NoProfileLoadTime",
     "-ExecutionPolicy RemoteSigned",
     "-Command ",
   }, " ")
-  opt.shellredir = "| Out-File -Encoding UTF8"
-  opt.shellpipe = "| Out-File -Encoding UTF8"
-  opt.shellquote = ""
-  opt.shellxquote = ""
+  vim.opt.shellredir = "| Out-File -Encoding UTF8"
+  vim.opt.shellpipe = "| Out-File -Encoding UTF8"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
 end
